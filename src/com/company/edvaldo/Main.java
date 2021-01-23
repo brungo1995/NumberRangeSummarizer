3d41d3931347b833332bedb715131fb07ec05982
edvaldo <brungo1995@gmail.com>
2021-01-23 17:59:01 +0200

    simplified code

--- src/com/company/edvaldo/Main.java
+++ src/com/company/edvaldo/Main.java
@@ -8,8 +8,12 @@ import java.util.Collection;
 public class Main {
 
     public static void main(String[] args) {
-//        String input = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
-//        String input = "1,3,6,7,8,12,13,14,15,21,22,23,24,31,-1,-2,0";
+        String input = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
+//        String input = "1,3,6,7,8,12,13,14,-20,15,21,22,23,24,31,-1,-2,-3,0";
+//        String input = "1,3,6,7,8,12,13,14,-20,15,21,22,23,24,31,-1,-2,-3,0";
+//        String input = "-20,-1,-2,-3,0,5";
+//        String input = ",-5,-2,-1,2,3";
+//        String input = "20,19,-1,1,3,3,000,22,-20,-2,0,1,2";
 //        String input2 = "31,12,24,21,1,3,6,7,8,13,14,15,22,23,31,1,0,7,13,31";
 //        String input = "-1,-2,0,1,3,2,4,0";
 //        String input = "9,8,7,3,1,2,0,5";
@@ -27,16 +31,16 @@ public class Main {
 //        var input = new Object();
 //        String input = "sgsfgs000000012,,,,        2.36,000000,98";
 //        String input = "00000000001,20,......6,0.8,98";
-        String input = "1,3,4,5,002";
+//        String input = "1,3,4,5,002";
 
         try{
             NumberRangeSummarizer summarizer = new Summarizer();
             Collection<Integer> numbers = summarizer.collect(input);
-//            String result = summarizer.summarizeCollection(numbers);
-//            System.out.println(result);
+            String result = summarizer.summarizeCollection(numbers);
+            System.out.println(result);
         }catch (Exception ex){
             System.out.println(ex.getMessage());
-            System.out.println(ex.getCause());
+//            System.out.println(ex.getCause());
         }
 
     }
--- src/numberrangesummarizer/Summarizer.java
+++ src/numberrangesummarizer/Summarizer.java
@@ -5,36 +5,19 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Summarizer implements  NumberRangeSummarizer{
+    private String ranges = "";
 
     @Override
     public Collection<Integer> collect(String input) throws InvalidInputException {
-        // FIRST THING MUST BE VALIDATION AND PREPARATION OF the INPUT
-        // IF WHAT COMES OUT OF THE VALIDATION FN IS NOT A PROPER INPUT RETURN AND PRINT THE MESSAGE
-//        if(!isValidInput()){
-//
-//        }
-        isValidInput(input);
+        Collection sortedNumbers;
+        input = validateInput(input);
+        input = removeNonNumericCharacters(input);
+        sortedNumbers = sortNumbersAndCreateCollection(input);
 
-//         input = validateInput(input);
-        // adding the numbers into a SET removes the need to check again for duplicates as SETS don't store duplicates
-        Collection<Integer> numbers = new HashSet<>();
-
-//        Stream.of(input.split(","))
-//                .map(Integer::parseInt)
-//                .sorted()
-//                .forEach(numbers::add);
-
-//        removes duplicates without a map
-//        List<Integer> withoutDupes = Stream.of(input.split(",")).stream()
-//                .distinct()
-//                .collect(Collectors.toList());
-
-        return  numbers;
-
+        return  sortedNumbers;
     }
 
-    private boolean isValidInput(String input) throws InvalidInputException {
-
+    private String validateInput(String input) throws InvalidInputException {
         // if null
         if(input == null)
             throw new NullPointerException("User passed null input");
@@ -48,147 +31,124 @@ public class Summarizer implements  NumberRangeSummarizer{
             throw new NumberFormatException("User passed invalid input format. Input should be a String");
         }
 
-        // check if input has special characters
-//        if(hasNonNumericValues(input)){
-//            throw new NumberFormatException("User passed non numeric values. Please only use numbers followed by commas");
-//        }
+        // remove all non numeric character excluding commas
+//        input = removeNonNumericCharacters(input);
 
-        // remove all non numeric character exclucing commas
-        input = removeNonNumericCharacters(input);
-
-        // at this point there might also still be invalid commas or invalid formatd numbers
-        input = formatNumbers(input);
-
-        System.out.println(input);
-        // check if string is made of numbers (if they are parsable)
-        // parseInt can throw a NumberFormatException
-        // check if values are comma delimited
-        // check for decimal numbers
-
-
-
-
-
-        return false;
+        return input;
     }
 
     private String removeNonNumericCharacters(String input){
-        return  input.replaceAll("[^0-9,.]", "");
-    }
+        // remove special all special characters and only leaves numeric values (including negative numbers)
+        // input = input.replaceAll("[^0-9,.]", "");
+//        input = input.replaceAll("[^\\d,.-]", "");
+        input = input.replaceAll("[^\\d,-]", "");
 
-    private String formatNumbers(String input){
-        List<Integer> list = new ArrayList<>();
-//              List<Integer> splitInputs = Stream.of(input.split(","))
+//        // removes spaces, fractional numbers, and blanks
+//        input = Stream.of(input.split(","))
 //                .filter(item -> !item.isEmpty()
-//                                && !item.isBlank()
-//                                && !item.contains("."))
+//                        && !item.isBlank()) //filter out decimal values
 //                .map(String::trim)
 //                .map(Integer::parseInt)
-//                .collect(Collectors.toList());
+//                .distinct() // removes duplicates
+//                .sorted()
+//                .map(Object::toString) //convert back to string
+//                .collect(Collectors.joining(",")); // join everything into a single string
+        return input;
+    }
 
-             String splitInputs = Stream.of(input.split(","))
+    private Collection<Integer> sortNumbersAndCreateCollection(String formattedInput){
+        Collection<Integer> sortedNumbers = new ArrayList<Integer>();
+
+        Stream.of(formattedInput.split(","))
                 .filter(item -> !item.isEmpty()
-                                && !item.isBlank()
-                                && !item.contains("."))
+                        && !item.isBlank()) //filter out decimal values
                 .map(String::trim)
                 .map(Integer::parseInt)
-                .map(Object::toString)
-                .collect(Collectors.joining(","));
+                .distinct() // removes duplicates
+                .sorted()
+                .forEach(sortedNumbers::add);
 
-
-//        List<String> splitInputs = Stream.of(input.split(","))
-//                .map(String::trim)
-//                .collect(Collectors.toList());
-//
-//        for(String item: splitInputs){
-//
-//            if(item.isEmpty() || item.isBlank()){
-//                continue;
-//            }
-//
-//            if(item.contains(".")){
-//                continue;
-//            }
-//
-//            list.add(Integer.valueOf(item));
-//        }
-
-
-        return  splitInputs;
-//        return  list.toString();
+                return sortedNumbers;
     }
-
-
     @Override
     public String summarizeCollection(Collection<Integer> input) {
-        Map<Integer, Integer> numberRanges = getRangesAsKeyValuePair(input);
-        return prepareFinalOutput(numberRanges);
+        String rangesFound = findRanges(input);
+        setRanges(rangesFound);
+        return getRanges();
     }
 
-    private Map<Integer,Integer> getRangesAsKeyValuePair(Collection<Integer> values){
-        ArrayList<Integer> newList= new ArrayList<>(values);
-        Map<Integer, Integer> map = new HashMap<>();
-
+    private String findRanges(Collection<Integer> masterColl){
+        ArrayList<Integer> values= new ArrayList<>(masterColl);
+        String output ="";
+        boolean isSequential = false;
         int rangeStart = 0;
-        boolean hasRangeStart = false;
+        int rangeEnd = 0;
 
-        for (int i = 0; i < newList.size(); i++) {
+        for (int i = 0; i < values.size(); i++) {
             int nextNum ;
-            int targetSum = newList.get(i) + 1;
+            int targetSum = values.get(i) + 1;
+            int currentValue = values.get(i);
 
-//            Last item
-            nextNum = i + 1 == newList.size() ? newList.get(newList.size() -1) : newList.get(i + 1);
+            // Last item
+            nextNum = i + 1 == values.size() ? values.get(values.size() -1) : values.get(i + 1);
 
-            if(nextNum == targetSum && hasRangeStart){
+            if(nextNum == targetSum && isSequential){
                 continue;
             }
 
-            if (nextNum == targetSum && !hasRangeStart){
-                rangeStart = newList.get(i);
-                hasRangeStart = true;
-                map.put(newList.get(i), null);
+            //add range start and the next sum is sequential
+            if (nextNum == targetSum && !isSequential){
+                rangeStart = currentValue;
+                isSequential = true;
                 continue;
             }
 
-            if(nextNum != targetSum && hasRangeStart){
+            // reached range end, next number is not sequential
+            if(nextNum != targetSum && isSequential){
                 //update the value of the map based on the key which is the start range
-                hasRangeStart = false;
-                map.replace(rangeStart, newList.get(i));
+                isSequential = false;
+                rangeEnd = currentValue;
+
+                // add the range to the output variable
+                output = output.concat(String.valueOf(rangeStart ))
+                        .concat("-")
+                        .concat(String.valueOf(rangeEnd ));
+
+                // if it is not the last item add "," to the output variable
+                if(!isLast(i + 1,  values.size()))
+                    output = output.concat(", ");
+
+                rangeStart = 0;
+                rangeEnd = 0;
+
                 continue;
             }
 
-            if(nextNum != targetSum && !hasRangeStart){
+            // in this case the next number is not sequential
+            if(nextNum != targetSum && !isSequential){
                 // in this case we don't have a range start neither the sum != current
-                map.put(newList.get(i), null);
+                output = output.concat(String.valueOf(currentValue ));
+
+                // if it is not the last item add "," to the output variable
+                if(!isLast(i + 1,  values.size()))
+                    output = output.concat(", ");
             }
 
         }
 
-        return  map;
+        return  output;
     }
 
-    private String prepareFinalOutput(Map<Integer,Integer> numberRanges){
-
-        String result = numberRanges.entrySet()
-                            .stream()
-                            .sorted(Map.Entry.comparingByKey())
-                            .map(entry -> {
-                            String range = "";
-
-                            // check if this entry has a value, if it has a value, then it is a range
-                            // key = start, value = end
-                            if(entry.getValue() != null){
-                                range = range.concat(String.valueOf(entry.getKey()));
-                                range = range.concat("-");
-                                range = range.concat(String.valueOf(entry.getValue()));
-                            }else {
-                                // if the entry has no value but has a key, then there is no continuity in the range
-                                range = range.concat(String.valueOf(entry.getKey()));
-                            }
-                            return range;
-                        })
-                        .collect(Collectors.joining(", "));
-
-        return result;
+    private boolean isLast(Integer currentIndex, Integer arrSize){
+        return  currentIndex == arrSize;
     }
+
+    private String getRanges(){
+        return ranges;
+    }
+
+    private void  setRanges(String ranges){
+        this.ranges = ranges;
+    }
+
 }
